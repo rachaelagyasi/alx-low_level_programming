cf9f13d7e4d03120fb34bf486115c24a35137d13
rachaelagyasi <rachaelagyasi>
2023-05-19 14:50:28 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry poipt.
+ * Description: Will print a message.
+ * Return: Always return 0.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
